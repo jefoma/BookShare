0ba840916d10d76ab1870aabaad985f5ccc14046
jefoma <jefoma@bemen3.cat>
2021-04-22 17:34:27 +0200

    Interfaz acabado Login/Registro/Perfil/Chats/Chat y Catalogo le falta una cosa

---
+++ app/src/main/java/com/example/bookshare/Catalogo.kt
@@ -0,0 +1,11 @@
+package com.example.bookshare
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Catalogo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
