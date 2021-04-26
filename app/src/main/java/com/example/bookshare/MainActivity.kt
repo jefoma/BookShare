910974aa5d15be6b2a4d23f44e7b338bb7bfdeb2
pefele30 <pafelo20@bemen3.cat>
2021-04-26 17:17:39 +0200

    Conectar FIrebase + Boton Registro

--- app/src/main/java/com/example/bookshare/MainActivity.kt
+++ app/src/main/java/com/example/bookshare/MainActivity.kt
@@ -1,11 +1,77 @@
 package com.example.bookshare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(),  View.OnClickListener {
+
+    private var login: Button? = null
+    private var sing_up: Button? = null
+    private var correoElectronico: EditText? = null
+    private var contraseña: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil_libro)
+        setContentView(R.layout.activity_main)
+
+
+        //Iniciamos los botenes y creamos sus listeners para crear acciones
+        login = findViewById<Button>(R.id.login);
+        sing_up = findViewById<Button>(R.id.sing_up);
+        correoElectronico = findViewById<EditText>(R.id.correoElectronico);
+        contraseña = findViewById<EditText>(R.id.contraseña);
+
+
+        login!!.setOnClickListener(this)
+        sing_up!!.setOnClickListener(this)
+        correoElectronico!!.setOnClickListener(this)
+        contraseña!!.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.login -> {
+                if (correoElectronico!!.text.isNotEmpty() && contraseña?.text!!.isNotEmpty()){
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(correoElectronico!!.text.toString(),
+                            contraseña!!.text.toString()).addOnCompleteListener{
+                        if (it.isSuccessful){
+                            val intent: Intent = Intent(this, Chats::class.java)
+                            startActivity(intent)
+
+                        }else{
+                            showAlert()
+                        }
+                    }
+
+                }
+
+            }
+            R.id.sing_up -> {
+                val intent: Intent = Intent(this, Registro::class.java)
+                startActivity(intent)
+            }
+        }
+
+    }
+
+    //Creamos una función para mostrar una alerta en caso de que se oproduzca un error
+    private fun showAlert(){
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error al intentar acceder")
+        builder.setPositiveButton("Aceptar",null)
+        val dialog: AlertDialog =builder.create()
+        dialog.show()
     }
 }
+
+
+
